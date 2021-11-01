4eb677401f16b279850fdcf45ba1d94658289819
AlexOdnov <alex.odnoralov@yandex.ru>
2021-11-01 18:29:54 +0300

    placement

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,87 @@
 <template>
-    <div id="app">
-        <div class="office">
-            <Map />
-            <SideMenu />
-        </div>
+  <div id="app">
+    <div class="office">
+      <Map @selectTable="setTableId" @unselectTable="unsetTableId" />
+      <side-menu :isUserOpened.sync="isUserOpened" :person="person" />
     </div>
+  </div>
 </template>
 
 <script>
-import Map from "./components/Map.vue";
-import SideMenu from "./components/SideMenu.vue";
+import Map from './components/Map.vue';
+import SideMenu from './components/SideMenu.vue';
+import people from './assets/data/people.json';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     Map,
     SideMenu,
+  },
+  data() {
+    return {
+      peoples: [],
+      isUserOpened: false,
+      activeTableId: null,
+    };
+  },
+  computed: {
+    person() {
+      return (
+        this.peoples.find((el) => el.tableId === this.activeTableId) || null
+      );
+    },
+  },
+  created() {
+    this.loadPeoples();
+  },
+  methods: {
+    loadPeoples() {
+      this.peoples = people;
+    },
+    setTableId(id) {
+      this.activeTableId = id;
+      this.isUserOpened = true;
+    },
+    unsetTableId() {
+      this.activeTableId = null;
+      this.isUserOpened = false;
+    },
   },
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    color: #2c3e50;
-    background-color: #fafafa;
-    padding: 24px;
-    box-sizing: border-box;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background-color: #fafafa;
+  padding: 24px;
+  box-sizing: border-box;
 }
 
 html,
 body,
 #app {
-    height: 100%;
+  height: 100%;
 }
 
 * {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 h3 {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 
 .office {
-    display: grid;
-    grid-template-columns: 1fr 320px;
-    border-radius: 6px;
-    border: 1px solid #ccd8e4;
-    height: 100%;
-    background: white;
-    max-width: 1500px;
-    margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 320px;
+  border-radius: 6px;
+  border: 1px solid #ccd8e4;
+  height: 100%;
+  background: white;
+  max-width: 1500px;
+  margin: 0 auto;
 }
 </style>
